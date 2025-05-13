4a4b384be2b2403e90b465e7a46374eaa43f023e
Mysterius79 <77966496+Mysterius79@users.noreply.github.com>
2025-05-13 12:25:09 +0200

    update code.py

--- code.py
+++ code.py
@@ -4,40 +4,38 @@ cp.pixels.brightness = 1
 delay = 0.04
 langsamer_modus = False
 
+    
+def change_delay(delay: float) -> float:
+    if cp.button_a:
+        time.sleep(0.5)
+        return delay + 0.1
+    elif cp.button_b:
+        time.sleep(0.5)
+        if delay > 0.04:
+            return delay - 0.1
+        else:
+            return 0.04 # wird benötigt, weil Gleitkommazahlenrundungsfehler auftreten können
+    return delay
+
 while True:
     #Da nichts in der Dokumentation steht, wie man Daten über das USB-Kabel senden und empfangen kann, fand ich es einfacher eine mp3 Datei temporär zu speichern und sie dann versuchen abzuspielen
     #Wenn es die Datei gibt, spielt es sie ab und fängt an zu blinken, wenn nicht dann sucht es einfach weiter
-    if cp.button_a:
-        delay = delay + 0.1
-        print(delay)
-        if delay < 0.15:
-            time.sleep(delay)
-    if cp.button_b:
-        if delay > 0.04:
-            delay = delay - 0.1
-            print(delay)
-        else:
-            delay = 0.04
+    delay = change_delay(delay)
+    print(delay)
     try:
         cp.play_mp3("trigger.mp3")
         cp.pixels.brightness = 1.0
     except:
         cp.pixels.brightness = 0.07
         cp.pixels.fill(0xFFFFFF)
+        delay = change_delay(delay)
         time.sleep(1)
         continue
+    delay = change_delay(delay)
+    
     cp.pixels.fill(0xFFFFFF)
     time.sleep(delay)
-    if cp.button_a:
-        delay = delay + 0.1
-        print(delay)
-        if delay < 0.15:
-            time.sleep(delay)
-    if cp.button_b:
-        if delay > 0.04:
-            delay = delay - 0.1
-            print(delay)
-        else:
-            delay = 0.04
+    delay = change_delay(delay)
+    
     cp.pixels.fill(0)
     time.sleep(delay)
